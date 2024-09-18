0a8f9c8b9aec8ba08c37e980c1a865f29208c8dd
Mateus Nuci <mateusnucicosta@gmail.com>
2024-09-18 09:48:07 -0300

    detail page incremented

--- lib/pages/details/movie_detail_page.dart
+++ lib/pages/details/movie_detail_page.dart
@@ -10,26 +10,62 @@ class MovieDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(movie.title),
+        title: Text(
+          movie.title,
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              movie.title,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Image.network(
+                'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 400,
+              ),
             ),
-            SizedBox(height: 400 ,child: Image.network(
-                    'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                    fit: BoxFit.fitWidth,
-                  ),),
-            const SizedBox(height: 10),
-            Text('Release Date: ${movie.releaseDate?.toLocal().toString().split(' ')[0] ?? 'N/A'}'),
-            const SizedBox(height: 10),
-            Text('Overview: ${movie.overview}'),
-
+            const SizedBox(height: 20),
+            Text(
+              'Release Date:',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[700]),
+            ),
+            Text(
+              movie.releaseDate?.toLocal().toString().split(' ')[0] ?? 'N/A',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Overview:',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[700]),
+            ),
+            Text(
+              movie.overview,
+              style: const TextStyle(fontSize: 16),
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Popularity:',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[700]),
+            ),
+            Text(
+              movie.popularity.toString(),
+              style: const TextStyle(fontSize: 16),
+            ),
           ],
         ),
       ),
